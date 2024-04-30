4df80dbdfb68a8d0064a6e0828ceff3955a8d051
YORNsoksunnara <163433515+YORNsoksunnara@users.noreply.github.com>
2024-05-01 01:38:48 +0700

    Update ipe.java

--- ipe.java
+++ ipe.java
@@ -1,25 +1,18 @@
 package hi;
 import java.util.Scanner;
 public class ipe {
-
-	
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         // Prompt the user to enter quantity
         System.out.print("Enter the quantity: ");
         int quantity = scanner.nextInt();
-        
         // Prompt the user to enter price
         System.out.print("Enter the price per item: ");
         double price = scanner.nextDouble();
-        
         // Calculate the total price
-        double total = quantity * price;
-        
+        double total = (quantity * price);  
         // Display the total price
-        System.out.println("Total price: " + total);
-        
+        System.out.println("Total price:$ " + total);
         // Close the scanner
         scanner.close();
     }
